0f4b6be9dce4e2fa3591cbcb59a913d57660ad23
Huan XIE <61042731+thehuanxie@users.noreply.github.com>
2022-09-19 13:33:26 +0200

    Update guess.php

--- guess.php
+++ guess.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kidz Tse_bf0dc199</title>
-    <!-- Do not change this file - add your CSS styling 
-       rules to the blocks.css file included below -->
-    <!-- <link type="text/css" rel="stylesheet" href="blocks.css"> -->
 </head>
 
 <body>
